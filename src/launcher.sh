1c21111e8e51522399283df83895a078fa5c1d7a
WRFitch <WRFitch@outlook.com>
2020-02-16 13:59:24 +0000

    replaced python script with simple bash runner

--- src/launcher.sh
+++ src/launcher.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
-# launcher.sh
+# launcher.sh 
+# 
+# first arg is project root 
 
-sudo python3 /src/AirQuality/src/run_sensor.py
+if [ -z "$1" ]
+then 
+    echo "missing project root"
+    return 1
+fi
+
+PROJECT_ROOT=$1
+
+arduino --upload --verbose-upload ${PROJECT_ROOT}/src/airquality/airquality.ino
+cu -l /dev/ttyACM0 -s 9600 >> "${PROJECT_ROOT}/logs/$(date +%Y_%m_%d).log"
